8da00d8a6581ae01f1fd22c60ee9c6d90b47110d
WeakBoss <2366507496@qq.com>
2018-03-21 11:11:11 +0800

    update

--- Clock.cpp
+++ Clock.cpp
@@ -1,11 +1,126 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
+using namespace std;
 class Clock {
+public:
+	Clock(int h, int m, int s);
+	Clock();
+	~Clock() {};
+	void tick();
+	void show();
+	void adjust();
+	void alarm();
+	void system();
+	void rang();
+private:
+	time_t timep;
+	time_t timea;
+	bool is_alarm;
+	bool is_rang;
+	int rang_time;
 };
-
+Clock::Clock(int h, int m, int s) {
+	time(&timep);
+	struct tm*p = localtime(&timep);
+	p->tm_hour = h;
+	p->tm_min = m;
+	p->tm_sec = s;
+	timep = mktime(p);
+	timep++;
+	is_alarm = false;
+	is_rang=false;
+	rang_time= 60;
+}
+Clock::Clock() {
+	time(&timep);
+	timep++;
+	is_alarm =is_rang= false;
+	rang_time = 60;
+}
+void Clock::tick() {
+	timep++;
+}
+void Clock::show() {
+	cout << ctime(&timep)<<endl;
+	
+}
+void Clock::system() {
+	if (_kbhit() != 0) {
+		cout << "选择" << endl << "1 调整时间 2 设闹钟" << endl;
+		int key;
+		cin >> key;
+		switch (key) {
+		case 1:
+			adjust();
+			break;
+		case 2:
+			alarm();
+			break;
+		}
+	}
+	if (is_alarm) {
+		struct tm*p = localtime(&timea);
+		cout << "alarm:" << p->tm_hour << ":" << p->tm_min << ":" << p->tm_sec <<"\t";
+	}
+	else {
+		cout << "no alarm \t" ;
+	}
+	if (is_rang) {
+		cout << "ranging\t" <<"letf "<<rang_time<<" seconds"<< endl;
+	}
+	else {
+		cout << "no rang" << endl;
+	}
+	if (timep == timea) {
+		is_rang = true;
+	}
+	if (rang_time == 0) {
+		is_alarm = is_rang = false;
+		rang_time = 60;
+	}
+}
+void Clock::adjust() {
+	
+	
+		cout << "输入  时 分 秒" << endl;
+		int h, m, s;
+		cin >> h >> m >> s;
+		struct tm*p = localtime(&timep);
+		p->tm_hour = h;
+		p->tm_min = m;
+		p->tm_sec = s;
+		timep = mktime(p);
+}
+void Clock::alarm() {
+	    cout << "输入  时 分 秒" << endl;
+		int h, m, s;
+		cin >> h >> m >> s;
+		time(&timea);
+		struct tm*p = localtime(&timea);
+		p->tm_hour = h;
+		p->tm_min = m;
+		p->tm_sec = s;
+		timea = mktime(p);
+		is_alarm = true;
+		timep += 5;
+}
+void Clock::rang() {
+	if (is_rang) {
+		cout << '\a';
+		rang_time--;
+	}
+}
 int main() {
-  Clock ck;
-  ck.tick;
-  ck.show();
-  ck.adjust(16,45,00);
-  ck.show();
-  return 0;
+	Clock ck;
+	while (true) {
+		ck.show();
+		cout << "调整时间或设闹钟 请按任意键" << endl;
+		ck.tick();
+		ck.system();
+		ck.rang();
+		Sleep(890);
+		system("cls");
+	}
 }
--- Date.cpp
+++ Date.cpp
@@ -1,31 +1,109 @@
+#include <iostream>
+using namespace std;
 class Date {
 public:
-  Date(int y, int m, int d);
-  ~Date();
+	Date(int y, int m, int d);
+	~Date();
 
-  void nextDay();
-  void previousDay();
-  bool equal(Date other);
-  void show();
+	void nextDay();
+	void previousDay();
+	bool equal(Date other);
+	void show();
+	int getyear() {
+		return year;
+	}
+	int getmonth() {
+		return month;
+	}
+	int getday() {
+		return day;
+	}
 private:
-  int year;
-  int month;
-  int day;
+	int year;
+	int month;
+	int day;
 };
+Date::Date(int y, int m, int d) :year(y), month(m), day(d) {
+	;
+}
+Date::~Date() {
+	;
+}
+void Date::nextDay() {
+	if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day == 31) {
+		month++;
+		day = 1;
+		if (month == 12) {
+			year++;
+			month = 1;
+		}
+	}
+	else if (( month == 4 || month == 6 || month == 9 || month == 11) && day == 30) {
+		month++;
+		day = 1;
+	}
+	else if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) && day == 29) {
+		month++;
+		day = 1;
+	}
+	else if (month == 2 && year % 4 != 0 && day == 28) {
+		month++;
+		day = 1;
+	}
+	else {
+		day++;
+	}
 
+}
+void Date::previousDay() {
+	if ((month == 2 || month == 4 || month == 6 || month == 8 || month == 9 || month == 11 || month == 1) && day == 1) {
+		month--;
+		day = 31;
+		if (month == 1) {
+			year--;
+			month = 12;
+		}
+	}
+	else if (( month == 5 || month == 6 || month == 10 || month == 12) && day == 1) {
+		month--;
+		day = 30;
+	}
+	else if (month == 3 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) && day == 1) {
+		month--;
+		day = 29;
+	}
+	else if (month == 3 && year % 4 != 0 && day == 1) {
+		month--;
+		day = 28;
+	}
+	else {
+		day--;
+	}
+}
+bool Date::equal(Date other) {
+	if (year == other.getyear() && month == other.getmonth() && day == other.getday()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+void Date::show() {
+	cout << year << "." << month << "." << day << endl;
+}
 int main() {
-  Date today(2018,3,20);
-  Date day(2018,3,20);
-  day.show();
-  for(int i=0;i<30;i++)
-    day.nextDay();
-  day.show();
-  for(int i=0;i<30;i++)
-    day.previousDay();
-  day.show();
-  if(day.equal(today))
-    printf("Very Good!\n");
-  else
-    printf("Very Bad!\n");
-  return 0;
+	Date today(2018, 3, 20);
+	Date day(2018, 3, 20);
+	day.show();
+	for (int i = 0;i<30;i++)
+		day.nextDay();
+	day.show();
+	for (int i = 0;i<30;i++)
+		day.previousDay();
+	day.show();
+	if (day.equal(today))
+		printf("Very Good!\n");
+	else
+		printf("Very Bad!\n");
+	return 0;
 }
