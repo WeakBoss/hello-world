75ddcbefd0ea778e925f26bb5f56acebf5828d9a
WeakBoss <2366507496@qq.com>
2018-03-27 17:46:07 +0800

    超大素数

--- Prime.cpp
+++ Prime.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 class Prime {
 public:
   Prime(int num);
@@ -8,16 +9,63 @@ public:
   Prime sumBit();
   Prime multiBit();
   Prime sqaureSumBit();
+  int Get();
 private:
-  const int mNum;
+   int mNum;
 };
-
+Prime::Prime(int num){
+  this->mNum=num; 
+} 
+Prime::~Prime(){
+  ;
+} 
+bool Prime::isPrime(){
+  for(int i=2;i<mNum;i++){
+     if(mNum%i==0) return false; 
+  }
+  return true;
+}
+Prime Prime::sumBit(){
+   int sumbit=0;
+   int temp=mNum;
+   while(temp>0){
+     sumbit=sumbit+temp%10;
+     temp/=10;
+   }
+   Prime pre(sumbit);
+   return pre; 
+}
+Prime Prime::multiBit(){
+   int multibit=1;
+   int temp=mNum;
+   while(temp>0){
+     multibit=multibit*(temp%10);
+	 temp/=10; 
+   } 
+   Prime pre(multibit);
+   return pre; 
+} 
+Prime Prime::sqaureSumBit(){
+   int sqauresumbit=0;
+   int temp=mNum;
+   while(temp>0){
+     sqauresumbit=sqauresumbit+(temp%10)*(temp%10);
+	 temp/=10; 
+   } 
+   Prime pre(sqauresumbit);
+   return pre; 
+} 
+int Prime::Get(){
+   return mNum;
+} 
 int main() {
-  Prime pm(113);
+  for(int i=113;i<=999;i++){ 
+  Prime pm(i); 
   Prime sb = pm.sumBit();
   Prime mb = pm.multiBit();
   Prime ssb = pm.sqaureSumBit();
   if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
+    std::cout<<pm.Get()<<std::endl; //to do sth
+}
   return 0;
 }
